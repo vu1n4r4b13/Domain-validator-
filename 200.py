20b9caebd747c6566cb4b9c8ee0cec1e9e9c3fdd
vu1n4r4b13 <75870938+vu1n4r4b13@users.noreply.github.com>
2023-04-30 16:17:20 +0530

    Update 200.py

--- 200.py
+++ 200.py
@@ -15,7 +15,7 @@ except ModuleNotFoundError:
     from tqdm import tqdm
     from termcolor import colored
 
-# Define your name as ASCII art
+# art
 name = r"""
              __          ___        ___ _     __   _____ 
             /  |        /   |      /   | |   /  | |____ |
@@ -27,27 +27,25 @@ __   ___   _`| | _ __  / /| |_ __ / /| | |__ `| |     / /
                                    twitter:@intuneric_boy
 """
 
-# Define a list of colors to choose from
 colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
-# Choose a random color for your name
 color = colors[hash(name) % len(colors)]
 
-# Print your name in the chosen color
 print(colored(name, color))
 
-# Get input and output file names
+#  input and output file names
 input_file = input("Enter the name of the input file: ")
 output_file = input("Enter the name of the output file: ")
 
-# Read in domains from input file
+# Reading domains 
 with open(input_file, 'r') as f:
     domains = f.read().splitlines()
 
-# Initialize list of reachable domains
+# list of reachable domains
 reachable_domains = []
 
 # Check each domain and append reachable domains to list
+
 for domain in tqdm(domains, desc="Checking domains", unit="domains"):
     try:
         response = requests.get('http://' + domain, timeout=5)
@@ -56,9 +54,10 @@ for domain in tqdm(domains, desc="Checking domains", unit="domains"):
     except:
         pass
 
-# Write reachable domains to output file
+# output in file
 with open(output_file, 'w') as f:
     f.write('\n'.join(reachable_domains))
 
-# Print confirmation message
+# exit
+
 print('Done. Reachable domains saved to', output_file)
